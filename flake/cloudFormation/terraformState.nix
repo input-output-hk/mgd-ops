d79102d17b23b6501f4ae00b8656c18f51b2f189
John Lotoski <john.lotoski@iohk.io>
2023-07-27 19:14:41 -0500

    imp: add cf KMS key and alias -- kmsKey

--- flake/cloudFormation/terraformState.nix
+++ flake/cloudFormation/terraformState.nix
@@ -3,6 +3,32 @@
   Description = "Terraform state handling";
 
   Resources = {
+    kmsKey = {
+      Type = "AWS::KMS::Key";
+      Properties =  {
+        KeyPolicy."Fn::Sub" = builtins.toJSON {
+          Version = "2012-10-17";
+          Statement = [
+            {
+              Action = "kms:*";
+              Effect = "Allow";
+              Principal.AWS = "arn:aws:iam::\${AWS::AccountId}:root";
+              Resource = "*";
+              Sid = "Enable admin use and IAM user permissions";
+            }
+          ];
+        };
+      };
+    };
+
+    kmsKeyAlias = {
+      Type = "AWS::KMS::Alias";
+      Properties = {
+        AliasName = "alias/kmsKey";
+        TargetKeyId.Ref = "kmsKey";
+      };
+    };
+
     S3Bucket = {
       Type = "AWS::S3::Bucket";
       DeletionPolicy = "Retain";
