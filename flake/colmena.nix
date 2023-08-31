9f9917c8c2acf25b4d377ad9a4ab15a27bceebac
Michael Fellinger <michael.fellinger@iohk.io>
2023-08-31 19:31:35 +0200

    client-eu-19 -> explorer

--- flake/colmena.nix
+++ flake/colmena.nix
@@ -35,7 +35,16 @@ in {
         lib.genList (mkNodeN nameFormat wgIpFormat imports) count
       );
 
-    eu-central-1.aws.region = "eu-central-1";
+    eu-central-1b.aws = {
+      region = "eu-central-1";
+      instance.availability_zone = "eu-central-1b";
+    };
+
+    eu-central-1c = {
+      aws.region = "eu-central-1";
+      aws.instance.availability_zone = "eu-central-1c";
+    };
+
     us-east-1.aws.region = "us-east-1";
     ap-southeast-2.aws.region = "ap-southeast-2";
 
@@ -54,10 +63,10 @@ in {
       meta.nixpkgs = import inputs.nixpkgs {system = "x86_64-linux";};
       defaults.imports = [common nixos-23-05];
     }
-    // (mkNode "leader" "10.200.0.1" [eu-central-1 r5-xlarge nomad-server (ebs 40)])
-    // (mkNode "deployer" "10.200.0.2" [eu-central-1 c5-9xlarge deployer (ebs 2000)])
-    // (mkNode "client-eu-19" "10.200.1.19" [eu-central-1 m5-4xlarge nomad-client (ebs 40)])
-    // (mkNodes 18 "client-eu-%02d" "10.200.1.%d" [eu-central-1 c5-2xlarge nomad-client (ebs 40)])
+    // (mkNode "leader" "10.200.0.1" [eu-central-1c r5-xlarge nomad-server (ebs 40)])
+    // (mkNode "deployer" "10.200.0.2" [eu-central-1b c5-9xlarge deployer (ebs 2000)])
+    // (mkNode "explorer" "10.200.1.19" [eu-central-1b m5-4xlarge nomad-client (ebs 40)])
+    // (mkNodes 18 "client-eu-%02d" "10.200.1.%d" [eu-central-1b c5-2xlarge nomad-client (ebs 40)])
     // (mkNodes 17 "client-ap-%02d" "10.200.2.%d" [ap-southeast-2 c5-2xlarge nomad-client (ebs 40)])
     // (mkNodes 17 "client-us-%02d" "10.200.3.%d" [us-east-1 c5-2xlarge nomad-client (ebs 40)]);
 }
