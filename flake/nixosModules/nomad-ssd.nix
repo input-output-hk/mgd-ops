080bf6cbf93183b72b5ff5158ff5b5eaacdc4e76
Shea Levy <shea@shealevy.com>
2024-03-15 10:01:10 -0400

    Make the nomad-ssd machine capable of "benchmark" Nix builds.
    Only used for doing local builds directly on the node, can be removed
    once it's wired up properly.

--- flake/nixosModules/nomad-ssd.nix
+++ flake/nixosModules/nomad-ssd.nix
@@ -1,11 +1,14 @@
 {
   flake.nixosModules.nomad-ssd = {
+    # Begin Remove when tooling is multi-cluster
     users.users.shlevy = {
       isNormalUser = true;
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID/fJqgjwPG7b5SRPtCovFmtjmAksUSNg3xHWyqBM4Cs shlevy@shlevy-laptop"
       ];
     };
+    nix.settings.system-features = [ "benchmark" ];
+    # End Remove when tooling is multi-cluster
 
     services.nomad.settings.client.host_volume = {
       "ssd1".path = "/ssd1";
